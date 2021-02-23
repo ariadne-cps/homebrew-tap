53ff69df890252fdcf01414c69cb2ae398b055d9
Luca Geretti <luca.geretti@gmail.com>
2021-02-23 12:01:10 +0100

    Create basic formula info

---
+++ Formula/ariadne.rb
@@ -0,0 +1,9 @@
+class Ariadne < Formula
+  desc "Ariadne C++ library for formal verification"
+  homepage "https://github.com/ariadne-cps/ariadne"
+  url "https://github.com/ariadne-cps/ariadne/archive/v2.0.tar.gz"
+  sha256 "944bc0436de6714e078ef44ff51d63f9ae4fbf968150ccca4cac504c731af462"
+  license "GPL-3"
+  head "https://github.com/ariadne-cps/ariadne.git"
+
+end

e194108114a7e2cd5f03d9e8ff7b7012a5208593
Luca Geretti <luca.geretti@gmail.com>
2021-03-05 18:06:31 +0100

    Use v2.1-rc2

--- Formula/ariadne.rb
+++ Formula/ariadne.rb
@@ -1,7 +1,7 @@
 class Ariadne < Formula
   desc "C++ library for formal verification of cyber-physical systems"
   homepage "https://github.com/ariadne-cps/ariadne"
-  url "https://github.com/ariadne-cps/ariadne/archive/2.1-rc2.tar.gz"
+  url "https://github.com/ariadne-cps/ariadne/archive/v2.1-rc2.tar.gz"
   sha256 "75886fbf6d4606efbb4145cb3ae0b11bd7bf0112db16a25dab483c1b5022e41e"
   license "GPL-3.0"
   head "https://github.com/ariadne-cps/ariadne.git"

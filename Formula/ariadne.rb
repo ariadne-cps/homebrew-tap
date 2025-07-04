3b4687d0ef6dbea80cabf4c57d5cfc5536c3bec3
Luca Geretti <luca.geretti@gmail.com>
2025-07-04 14:12:00 +0200

    Update formula for v2.5.3.tar.gz

--- Formula/ariadne.rb
+++ Formula/ariadne.rb
@@ -1,33 +1,32 @@
 class Ariadne < Formula
   desc "C++ library for formal verification of cyber-physical systems"
   homepage "https://github.com/ariadne-cps/ariadne"
-  url "https://github.com/ariadne-cps/ariadne/archive/v2.4.1.tar.gz"
-  sha256 "14e9dfe546c923ecb569349c25d304d273bc30e79037f68bc0e03522b24c9ae4"
+  url "https://github.com/ariadne-cps/ariadne/archive/v2.5.3.tar.gz"
+  sha256 "9b21878a24263a100af45d9e964af219fd6272e59d22541216288e5c808e3e0a"
   license "GPL-3.0"
   head "https://github.com/ariadne-cps/ariadne.git"
 
   bottle do
     root_url "https://www.ariadne-cps.org/cellar"
-    sha256 cellar: :any, big_sur: "f3af9774252af8ff22f5f72b800bbfa328757d90527f8b9bec1d1dacc5c98d56"
+    sha256 cellar: :any, sequoia: "9b21878a24263a100af45d9e964af219fd6272e59d22541216288e5c808e3e0a"
   end
 
   depends_on "cmake" => :build
-  depends_on "gcc@10" => :build
   depends_on "ninja" => :build
   depends_on "cairo"
   depends_on "gmp"
   depends_on "gnuplot"
   depends_on "mpfr"
-  depends_on "python@3.9"
+  depends_on "python@3"
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-G", "Ninja", "-DCMAKE_CXX_COMPILER=g++-10", "-DCMAKE_BUILD_TYPE=Release",
+      system "cmake", "..", "-G", "Ninja", "-DCMAKE_BUILD_TYPE=Release",
              "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DPYTHON_BINDINGS_INSTALL_DIR=libexec"
       system "cmake", "--build", ".", "--target", "install", "--parallel"
     end
 
-    python_version = Language::Python.major_minor_version Formula["python@3.9"].bin/"python3"
+    python_version = Language::Python.major_minor_version Formula["python@3"].bin/"python3"
     (lib/"python#{python_version}/site-packages/homebrew-ariadne.pth").write <<~EOS
       import site; site.addsitedir('#{libexec}')
     EOS
